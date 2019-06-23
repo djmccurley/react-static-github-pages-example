5d7d9d15bb44452d6c730e8ca13778da30af692f
Dan McCurley <djmccurley@gmail.com>
2019-06-23 11:19:32 -0500

    Updates

--- static.b2079494.js
+++ static.b2079494.js
@@ -67,7 +67,7 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "https://ninaolo.github.io/react-static-github-pages-example/";
+/******/ 	__webpack_require__.p = "https://djmccurley.github.io/react-static-github-pages-example/";
 /******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(__webpack_require__.s = 10);
@@ -1528,4 +1528,4 @@ function toComment(sourceMap) {
 /***/ })
 /******/ ]);
 });
-//# sourceMappingURL=static.9de67ba4.js.map
+//# sourceMappingURL=static.b2079494.js.map
